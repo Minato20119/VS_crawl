887929e434b890bdb0652c0e6d3ef4509487f014
Minato20119 <31340334+Minato20119@users.noreply.github.com>
2017-11-14 17:23:22 +0700

    Update connectDB.go

--- connectDB.go
+++ connectDB.go
@@ -1,7 +1,6 @@
 package crawVirusshare
 
 import (
-	"fmt"
 	"gopkg.in/mgo.v2"
 	"io/ioutil"
 	"log"
@@ -18,6 +17,8 @@ type Configuration struct {
 	Password   string `toml:"Password"`
 	Database   string `toml:"Database"`
 	Collection string `toml:"Collection"`
+	Source     string `toml:"Source"`
+	Worker     int    `toml:"Worker"`
 }
 
 func LoadFileConfig(path string) Configuration {
@@ -25,32 +26,28 @@ func LoadFileConfig(path string) Configuration {
 	if err != nil {
 		log.Fatal("Config File Missing. ", err)
 	}
-
 	var config Configuration
-	_, err = toml.Decode(string(file), &config)
-	if err != nil {
-		log.Fatal("Config Parse Error: ", err)
-	}
 
+	toml.Decode(string(file), &config)
 	return config
 }
 
-func connectMongoDB() (*mgo.Collection, *mgo.Session){
+func connectMongoDB() (*mgo.Collection, *mgo.Session) {
 	infoMongoDb := &mgo.DialInfo{
 		Addrs:    []string{config.HostName + ":" + config.PortName},
 		Timeout:  60 * time.Second,
 		Database: config.Database,
 		Username: config.Username,
 		Password: config.Password,
-		Source:   "admin",
+		Source:   config.Source,
 	}
 
 	session, err := mgo.DialWithInfo(infoMongoDb)
 	if err != nil {
-		fmt.Println("Error connect database...")
+		log.Println("Error connect database...")
 		panic(err)
 	}
-	//defer session.Close()
+
 	session.SetMode(mgo.Monotonic, true)
 	data := session.DB(config.Database).C(config.Collection)
 	return data, session
