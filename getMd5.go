6a341075b19134aa2e6e4ca616e115f1fae89971
Minato20119 <31340334+Minato20119@users.noreply.github.com>
2017-11-14 17:22:23 +0700

    Fix cmt getMd5

--- getMd5.go
+++ getMd5.go
@@ -1,18 +1,18 @@
 package crawVirusshare
 
 import (
-	"fmt"
+	"log"
 	"regexp"
 )
 
 var RegexMd5 = regexp.MustCompile("[0-9|a-z]{32}")
-
 //
 //const url = "https://virusshare.com/hashes/VirusShare_00279.md5"
 func getMd5(url string) []string {
 	source, err := getSourceUrl(url)
 	if err != nil {
-		fmt.Println("Error get Source Url from getMd5: ", err)
+		log.Println(err)
+		return nil
 	}
 	md5 := RegexMd5.FindAllString(source, -1)
 
