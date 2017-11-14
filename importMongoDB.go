0b7b7585d40089a8fa1f345a8c6482fc218f3390
Minato20119 <31340334+Minato20119@users.noreply.github.com>
2017-11-14 17:21:12 +0700

    Fix cmt importMongoDB

--- importMongoDB.go
+++ importMongoDB.go
@@ -1,11 +1,12 @@
 package crawVirusshare
 
 import (
-	"fmt"
-	"runtime"
+	"log"
 	"sync"
 	"gopkg.in/mgo.v2"
 )
+
+var data *mgo.Collection
 
 type Virusshare struct {
 	Type   string
@@ -14,35 +15,21 @@ type Virusshare struct {
 }
 
 var wg sync.WaitGroup
-var countLink = 0
 
-func importDB(data *mgo.Collection, md5 []string, start int, end int) {
-
-	for lineMd5 := start; lineMd5 < end; lineMd5++ {
-		err := data.Insert(&Virusshare{"md5", md5[lineMd5], "virusshare.com"})
-		if err != nil {
-			fmt.Println("Error Insert Into Database: ", err)
-		}
-	}
-}
-
-func allLinkMd5(data *mgo.Collection, linkMd5 []string, start int, end int) {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+func importDB(linkMd5 []string, start int, end int) {
 
 	for lineLinkMd5 := start; lineLinkMd5 < end; lineLinkMd5++ {
 		md5 := getMd5(linkMd5[lineLinkMd5])
 		lenMd5 := len(md5)
-		fmt.Println(countLink)
-		countLink++
-		fmt.Println(linkMd5[lineLinkMd5])
 
-		workers := 20
-		temp := lenMd5 / workers
-		wg.Add(20)
+		log.Println(linkMd5[lineLinkMd5])
 
-		for worker := 0; worker < 19; worker++ {
-			go importDB(data, md5, worker*temp, (worker+1)*temp)
+		for lineMd5 := 0; lineMd5 < lenMd5; lineMd5++ {
+			err := data.Insert(Virusshare{"md5", md5[lineMd5], "virusshare.com"})
+			if err != nil {
+				log.Println(err)
+				return
+			}
 		}
-		importDB(data, md5, 19*temp, lenMd5)
 	}
 }
