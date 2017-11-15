766fdc649aad2fe08c59a794ce2a11c7456ac0a2
Minato20119 <31340334+Minato20119@users.noreply.github.com>
2017-11-15 17:57:34 +0700

    Update getMd5.go

--- getMd5.go
+++ getMd5.go
@@ -6,8 +6,7 @@ import (
 )
 
 var RegexMd5 = regexp.MustCompile("[0-9|a-z]{32}")
-//
-//const url = "https://virusshare.com/hashes/VirusShare_00279.md5"
+
 func getMd5(url string) []string {
 	source, err := getSourceUrl(url)
 	if err != nil {
@@ -15,6 +14,5 @@ func getMd5(url string) []string {
 		return nil
 	}
 	md5 := RegexMd5.FindAllString(source, -1)
-
 	return md5
 }
